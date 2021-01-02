6025346fcef14288b0bf465e8998d17e5df3427a
oaparfene <oaparfene@gmail.com>
2021-01-02 15:28:18 +0100

    menu and reset btns + lvl6

--- Levels/LevelNode.gd
+++ Levels/LevelNode.gd
@@ -8,3 +8,13 @@ func startLevel(levelName=LevelData.currentLevel):
 		childTileMap.free()
 	LevelData.setCurrentLevel(levelName)
 	add_child(load("res://Levels/"+levelName+".tscn").instance())
+
+
+func _on_btnReset_pressed():
+	startLevel()
+
+
+func _on_btnMenu_pressed():
+	for childTileMap in get_children():
+		childTileMap.free()
+	var _ERROR_CODE = get_tree().change_scene("res://MenuScene.tscn")
