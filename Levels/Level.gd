b772345fce2eeb317f9b998f9672669956d872aa
oaparfene <oaparfene@gmail.com>
2021-01-06 15:32:51 +0100

    random background generation

--- Levels/Level.gd
+++ Levels/Level.gd
@@ -1,8 +1,23 @@
 extends TileMap
 
+export var randomiseBackground:bool = true
+var rng = RandomNumberGenerator.new()
+var void_tile_id = 3
+var void_dots_tile_id_offset = 3
+
 var LevelNode = get_parent()
 
 func _ready():
 	Globals.initialiseObjectPhases()
 	Globals.initialiseButtonSignals()
 	LevelData.initialiseMoveCount()
+	randomiseBackgroundTiles()
+
+func randomiseBackgroundTiles():
+	if not randomiseBackground:
+		return
+	rng.randomize()
+	var tile_id_to_randomize = void_tile_id
+	var tile_array = get_used_cells_by_id(tile_id_to_randomize)
+	for tile in tile_array:
+		set_cellv(tile, void_tile_id + void_dots_tile_id_offset + rng.randi_range(0,5))
