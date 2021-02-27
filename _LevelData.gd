0acce2760311d187d1fcea692019c298aa02d64d
oaparfene <oaparfene@gmail.com>
2021-02-27 19:03:39 +0100

    levels reorganization and changes

--- _LevelData.gd
+++ _LevelData.gd
@@ -3,7 +3,7 @@ extends Node
 var level_complete:bool = false
 
 var current_level:String
-var levels_per_world := 15
+var levels_per_world := 10
 
 func set_current_level(levelName):
 	level_complete = false
@@ -12,7 +12,7 @@ func set_current_level(levelName):
 func set_level_next():
 	var nextLvlName = int(current_level.split("-")[1])+1
 	#print(SaveData.current_world)
-	if nextLvlName > 15:
+	if nextLvlName > levels_per_world:
 		Transition.unlock_new_world()
 	if nextLvlName < 10:
 		nextLvlName = "0" + str(nextLvlName)
